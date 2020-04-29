efb1efaddd03d43d8b6946b79e9035d839510721
Judith <judith.vballesteros@gmail.com>
2020-04-29 14:16:22 -0500

    Loading funtional modules

---
+++ mk-modules/mk-plink-maf/counter_variants.R
@@ -0,0 +1,35 @@
+## load libraries
+library("ggplot2")
+library("dplyr")
+
+## Read args from command line
+args = commandArgs(trailingOnly=TRUE)
+
+## Uncomment For debugging only
+## Comment for production mode only
+# args[1] <- "test/data/sampleWGS.maf.plink.tsv" ## variants in original file
+# args[2] <- "test/results/sampleWGS.maf.plink.bed" ## stem for output
+
+## Passing args to named objects
+variants_tsv <- args[1]
+stem <- args[2]
+output_file <- gsub(pattern = ".bed", replacement = ".svg", x = stem)
+
+## Make df
+variant_table.df <- read.table(file = variants_tsv, header = TRUE, sep = "\t", stringsAsFactors = F)
+
+#Make plot
+comparison.p <- ggplot(variant_table.df, aes(FILE, VARIANTS, fill=FILE)) +
+  geom_bar(stat="identity") +
+  geom_text(aes(label=VARIANTS), position=position_dodge(width=0.9), vjust=-0.25) +
+  labs(title = "Number of variants comparison") +
+  theme_bw() +
+  theme(legend.position="none")
+
+## Save plot
+ggsave(filename = output_file,
+       plot = comparison.p,
+       device = "svg",
+       width = 14.4,
+       height = 7.2,
+       units = "in", dpi = 300)
---
+++ mk-modules/mk-plink-maf/testmodule.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+## This small script runs a module test with the sample data
+
+###
+## environment variable setting
+export PLINK2="plink2"
+export MAF="0.05"
+export THREADS_PLINK="4"
+export FAM_ID="NAT_AMR"
+###
+
+echo "[>..] test running this module with data in test/data"
+## Remove old test results, if any; then create test/reults dir
+rm -rf test/results
+mkdir -p test/results
+echo "[>>.] results will be created in test/results"*.vcf
+## Execute runmk.sh, it will find the basic example in test/data
+## Move results from test/data to test/results
+## results files are *.bed, *.bim & *.fam without LD and maf filtered.
+bash runmk.sh \
+&& mv test/data/*.maf.plink.* test/results/ \
+&& echo "[>>>] Module Test Successful"
